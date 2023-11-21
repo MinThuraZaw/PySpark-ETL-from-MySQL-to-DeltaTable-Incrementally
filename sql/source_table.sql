a7aa693b4709d0871b1d268d0ace320ab5def062
Min Thura Zaw <minthurazaw619@gmail.com>
2023-11-21 21:26:37 +0700

    Create source_table.sql

---
+++ sql/source_table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE demo_database.members (
+  `id` bigint DEFAULT NULL,
+  `member_id` varchar(10),
+  `name` varchar(255),
+  `email` varchar(255),
+  `phone` varchar(20),
+  `favorite_store_id` int DEFAULT NULL,
+  `last_purchase_date` date DEFAULT NULL,
+  `member_type_rfm` varchar(20),
+  `member_category` varchar(20),
+  `status` int DEFAULT NULL,
+  `verified` int DEFAULT NULL,
+  `photo_id` int DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
